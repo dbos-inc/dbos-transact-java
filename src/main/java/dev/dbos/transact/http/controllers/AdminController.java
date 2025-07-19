681673068b4d45b9d5168e8b0d39555d116e0b0c
manojdbos <manoj.khangaonkar@dbos.dev>
2025-07-18 17:00:38 -0700

    AdminController skeleletal methods

--- src/main/java/dev/dbos/transact/http/controllers/AdminController.java
+++ src/main/java/dev/dbos/transact/http/controllers/AdminController.java
@@ -1,18 +1,101 @@
 package dev.dbos.transact.http.controllers;
 
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import dev.dbos.transact.workflow.ListWorkflowsInput;
+import dev.dbos.transact.workflow.StepInfo;
+import dev.dbos.transact.workflow.WorkflowStatus;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/")
 public class AdminController {
+
     @GET
     @Path("/healthz")
     @Produces(MediaType.TEXT_PLAIN)
     public String health() {
         return "Healthy";
     }
+
+    @GET
+    @Path("/deactivate")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String deactivate() {
+        return "deactivated";
+    }
+
+    @GET
+    @Path("/workflow-queues-metadata")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object workflowQueuesMetadata() {
+        return "queuesMetadata";
+    }
+
+    @GET
+    @Path("/workflows/{workflowId}/steps")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<StepInfo> ListSteps(@PathParam("workflowId") String workflowId) {
+        return new ArrayList<>() ;
+    }
+
+    @GET
+    @Path("/workflows/{workflowId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<WorkflowStatus> ListWorkflows(@PathParam("workflowId") String workflowId) {
+        return new ArrayList<>() ;
+    }
+
+    @POST
+    @Path("/recovery")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> recovery() {
+
+        return new ArrayList<>();
+    }
+
+    @POST
+    @Path("/workflows")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<WorkflowStatus> workflows( ListWorkflowsInput input) {
+
+        return new ArrayList<>();
+    }
+
+    @POST
+    @Path("/workflows/{workflowId}/restart")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void restart(@PathParam("workflowId") String workflowId) {
+
+
+    }
+
+    @POST
+    @Path("/workflows/{workflowId}/resume")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void resume(@PathParam("workflowId") String workflowId) {
+
+
+    }
+
+    @POST
+    @Path("/workflows/{workflowId}/fork")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void fork(@PathParam("workflowId") String workflowId) {
+
+
+    }
+
+    @POST
+    @Path("/workflows/{workflowId}/cancel")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void cancel(@PathParam("workflowId") String workflowId) {
+
+
+    }
+
 
 }
