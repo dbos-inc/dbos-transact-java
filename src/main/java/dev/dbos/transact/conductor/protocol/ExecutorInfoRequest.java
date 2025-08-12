025282e2412235a9d998fc36da90f0c3c7e6d4b2
Harry Pierson <harry.pierson@dbos.dev>
2025-08-12 16:07:17 -0700

    Handle executor info conductor request

--- src/main/java/dev/dbos/transact/conductor/protocol/ExecutorInfoRequest.java
+++ src/main/java/dev/dbos/transact/conductor/protocol/ExecutorInfoRequest.java
@@ -2,4 +2,12 @@ package dev.dbos.transact.conductor.protocol;
 
 public class ExecutorInfoRequest extends BaseMessage {
     // empty on purpose
+
+    public ExecutorInfoRequest() {
+    }
+
+    public ExecutorInfoRequest(String requestId) {
+        this.type = MessageType.EXECUTOR_INFO.getValue();
+        this.request_id = requestId;
+    }
 }
--- src/main/java/dev/dbos/transact/execution/DBOSExecutor.java
+++ src/main/java/dev/dbos/transact/execution/DBOSExecutor.java
@@ -59,6 +59,20 @@ public class DBOSExecutor {
         return config.getName();
     }
 
+    // Unfortunetly, the GlobalParams statics can not be mocked effectively,
+    // making it nearly impossible to components that depend on them in isolation.
+    // I've added these mockable-methods here on DBOSExecutor to enable testing in
+    // isolation
+    // TODO: move actual global params to DBOSExecutor
+
+    public String getExecutorId() {
+        return GlobalParams.getInstance().getExecutorId();
+    }
+
+    public String getAppVersion() {
+        return GlobalParams.getInstance().getAppVersion();
+    }
+
     public void setQueueService(QueueService queueService) {
         this.queueService = queueService;
     }
