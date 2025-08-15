1d94d073f2b774ddfb48852045f8114ad05962c9
Harry Pierson <harry.pierson@dbos.dev>
2025-08-15 12:35:17 -0700

    Revert "declare checked exception in ManualResetEvent"
    This reverts commit 1b023e61182b8648838e4fe89016846044269366.

--- src/test/java/dev/dbos/transact/queue/ConcurrencyTestServiceImpl.java
+++ src/test/java/dev/dbos/transact/queue/ConcurrencyTestServiceImpl.java
@@ -17,7 +17,7 @@ public class ConcurrencyTestServiceImpl implements ConcurrencyTestService {
     }
 
     @Workflow(name = "blockedWorkflow")
-    public int blockedWorkflow(int i) throws InterruptedException {
+    public int blockedWorkflow(int i) {
         wfEvents.get(i).set();
         counter++;
         event.waitOne();
