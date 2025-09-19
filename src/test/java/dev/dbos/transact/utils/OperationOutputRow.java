dfe6cdb593355f95015240064eb2946f98277a13
Chuck B <chuck.bear@dbos.dev>
2025-09-19 10:55:11 -0400

    Spotlessify

--- src/test/java/dev/dbos/transact/utils/OperationOutputRow.java
+++ src/test/java/dev/dbos/transact/utils/OperationOutputRow.java
@@ -4,21 +4,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public record OperationOutputRow(
-        String workflowId,
-        int functionId,
-        String output,
-        String error,
-        String functionName,
-        String childWorkflowId
-) {
+    String workflowId,
+    int functionId,
+    String output,
+    String error,
+    String functionName,
+    String childWorkflowId) {
 
-    public OperationOutputRow(ResultSet rs) throws SQLException {
-        this(
-                rs.getString("workflow_uuid"),
-                rs.getInt("function_id"),
-                rs.getString("output"),
-                rs.getString("error"),
-                rs.getString("function_name"),
-                rs.getString("child_workflow_id"));
-    }
+  public OperationOutputRow(ResultSet rs) throws SQLException {
+    this(
+        rs.getString("workflow_uuid"),
+        rs.getInt("function_id"),
+        rs.getString("output"),
+        rs.getString("error"),
+        rs.getString("function_name"),
+        rs.getString("child_workflow_id"));
+  }
 }
