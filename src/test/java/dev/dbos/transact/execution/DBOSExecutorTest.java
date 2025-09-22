c07464afba6b36987edca6d36988b50cc5b30b1a
Harry Pierson <harry.pierson@dbos.dev>
2025-09-22 15:12:27 -0700

    enable more tests

--- src/test/java/dev/dbos/transact/execution/DBOSExecutorTest.java
+++ src/test/java/dev/dbos/transact/execution/DBOSExecutorTest.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 class DBOSExecutorTest {
 
   private static DBOSConfig dbosConfig;
