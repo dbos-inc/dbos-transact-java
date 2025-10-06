dfa4e372ac9ac35b41434a8f50c801eb41a50f4b
Harry Pierson <harrypierson@hotmail.com>
2025-10-06 12:19:35 -0700

    shutdown ExecutorServices on DBOS shutdown (#111)
    Fixes #109

--- src/main/java/dev/dbos/transact/execution/DBOSExecutor.java
+++ src/main/java/dev/dbos/transact/execution/DBOSExecutor.java
@@ -78,8 +78,8 @@ public class DBOSExecutor implements AutoCloseable {
   private RecoveryService recoveryService;
   private HttpServer httpServer;
   private Conductor conductor;
-  private ExecutorService executorService = Executors.newCachedThreadPool();
-  private ScheduledExecutorService timeoutScheduler = Executors.newScheduledThreadPool(2);
+  private ExecutorService executorService;
+  private ScheduledExecutorService timeoutScheduler;
   private final AtomicBoolean isRunning = new AtomicBoolean(false);
 
   public DBOSExecutor(DBOSConfig config) {
@@ -117,6 +117,9 @@ public class DBOSExecutor implements AutoCloseable {
                 .collect(Collectors.toList());
         this.appVersion = AppVersionComputer.computeAppVersion(registeredClasses);
       }
+
+      executorService = Executors.newCachedThreadPool();
+      timeoutScheduler = Executors.newScheduledThreadPool(2);
 
       systemDatabase = new SystemDatabase(config);
       systemDatabase.start();
@@ -178,6 +181,9 @@ public class DBOSExecutor implements AutoCloseable {
       queueService = null;
       systemDatabase.stop();
       systemDatabase = null;
+
+      timeoutScheduler.shutdownNow();
+      executorService.shutdownNow();
 
       this.workflowMap = null;
       this.dbos = null;
