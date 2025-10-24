7cb33c096ffc9e0137530e80418a3aa63d6c02c5
Chuck B <chuck.bear@dbos.dev>
2025-10-24 09:32:33 -0400

    Client / fork options

--- src/main/java/dev/dbos/transact/DBOSClient.java
+++ src/main/java/dev/dbos/transact/DBOSClient.java
@@ -17,8 +17,16 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * DBOSClient allows external programs to interact with DBOS apps via
+ *   direct system database access.
+ * Example interactions:
+ *   Start/enqueue a workflow, and get the result
+ *   Get events and send messages to the workflow
+ *   Manage workflows - list, fork, cancel, etc.
+ */
 public class DBOSClient implements AutoCloseable {
-  public class WorkflowHandleClient<T, E extends Exception> implements WorkflowHandle<T, E> {
+  private class WorkflowHandleClient<T, E extends Exception> implements WorkflowHandle<T, E> {
     private String workflowId;
 
     public WorkflowHandleClient(String workflowId) {
@@ -43,6 +51,12 @@ public class DBOSClient implements AutoCloseable {
 
   private final SystemDatabase systemDatabase;
 
+  /**
+   * Construct a DBOSClient, by providing system database access credentials
+   * @param url System database JDBC URL
+   * @param user System database user
+   * @param password System database credential / password
+   */
   public DBOSClient(String url, String user, String password) {
     var dataSource = SystemDatabase.createDataSource(url, user, password, 0, 0);
     systemDatabase = new SystemDatabase(dataSource);
@@ -53,6 +67,13 @@ public class DBOSClient implements AutoCloseable {
     systemDatabase.close();
   }
 
+  /**
+   * Options for enqueuing a workflow.
+   * It is necessary to specify the class and name of the workflow to enqueue,
+   *   as well as the queue to use.
+   * Other options, such as the workflow ID, queue options, and app version, are optional,
+   *  and should be set with `with` functions.
+   */
   public record EnqueueOptions(
       String workflowName,
       String queueName,
@@ -87,10 +108,16 @@ public class DBOSClient implements AutoCloseable {
       }
     }
 
+    /** Construct `EnqueueOptions` with a minimum set of required options */
     public EnqueueOptions(String className, String workflowName, String queueName) {
       this(workflowName, queueName, className, "", null, null, null, null, null);
     }
 
+    /**
+     * Specify the Java classname for the class containing the workflow to enqueue
+     * @param className Class containing the workflow to enqueue
+     * @return New `EnqueueOptions` with the class name set
+     */
     public EnqueueOptions withClassName(String className) {
       return new EnqueueOptions(
           this.workflowName,
@@ -104,6 +131,12 @@ public class DBOSClient implements AutoCloseable {
           this.priority);
     }
 
+    /**
+     * Specify the workflow ID for the workflow to be enqueued.  This is an
+     *  idempotency key for running the workflow.
+     * @param workflowId Workflow idempotency ID to use
+     * @return New `EnqueueOptions` with the workflow ID set
+     */
     public EnqueueOptions withWorkflowId(String workflowId) {
       return new EnqueueOptions(
           this.workflowName,
@@ -117,6 +150,13 @@ public class DBOSClient implements AutoCloseable {
           this.priority);
     }
 
+    /**
+     * Specify the app version for the workflow to be enqueued.  The
+     *   workflow will be executed by an executor with this app version.
+     * If not specified, the current app version will be used.
+     * @param appVersion Application version to use for executing the workflow
+     * @return New `EnqueueOptions` with the app version set
+     */
     public EnqueueOptions withAppVersion(String appVersion) {
       return new EnqueueOptions(
           this.workflowName,
@@ -130,6 +170,12 @@ public class DBOSClient implements AutoCloseable {
           this.priority);
     }
 
+    /**
+     * Specify a timeout for the workflow to be enqueued.
+     *   Timeout begins once the workflow is running; if it exceeds this it will be canceled.
+     * @param timeout Duration of time, from start, before the workflow is canceled.
+     * @return New `EnqueueOptions` with the timeout set
+     */
     public EnqueueOptions withTimeout(Duration timeout) {
       return new EnqueueOptions(
           this.workflowName,
@@ -143,6 +189,12 @@ public class DBOSClient implements AutoCloseable {
           this.priority);
     }
 
+    /**
+     * Specify a queue deduplication ID for the workflow to be enqueued.
+     *   Queue requests with the same deduplication ID will be consolidated.
+     * @param deduplicationId Queue deduplication ID
+     * @return New `EnqueueOptions` with the deduplication ID set
+     */
     public EnqueueOptions withDeduplicationId(String deduplicationId) {
       return new EnqueueOptions(
           this.workflowName,
@@ -156,6 +208,13 @@ public class DBOSClient implements AutoCloseable {
           this.priority);
     }
 
+    /**
+     * Specify an object instance name to execute the workflow.
+     *   If workflow objects are named, this must be specified to direct processing
+     *   to the correct instance.
+     * @param instName Instance name registered within `DBOS.registerWorkflows`
+     * @return New `EnqueueOptions` with the target instance name set
+     */
     public EnqueueOptions withInstanceName(String instName) {
       return new EnqueueOptions(
           this.workflowName,
@@ -169,6 +228,12 @@ public class DBOSClient implements AutoCloseable {
           this.priority);
     }
 
+    /**
+     * Specify priority.
+     *   Priority must be enabled on the queue for this to be effective.
+     * @param priority Queue priority; if `null`, priority '0' will be used.
+     * @return New `EnqueueOptions` with the priority set
+     */
     public EnqueueOptions withPriority(Integer priority) {
       return new EnqueueOptions(
           this.workflowName,
@@ -182,14 +247,27 @@ public class DBOSClient implements AutoCloseable {
           priority);
     }
 
+    /**
+     * Get the workflow ID that will be used
+     * @return The workflow idemptence ID
+     */
     @Override
     public String workflowId() {
       return workflowId != null && workflowId.isEmpty() ? null : workflowId;
     }
   }
 
+  /**
+   * Enqueue a workflow.
+   * @param <T> Return type of workflow function
+   * @param <E> Exception thrown by workflow function
+   * @param options `DBOSClient.EnqueueOptions` for enqueuing the workflow
+   * @param args Arguments to pass to the workflow function
+   * @return WorkflowHandle for retrieving workflow ID, status, and results
+   * @throws Exception
+   */
   public <T, E extends Exception> WorkflowHandle<T, E> enqueueWorkflow(
-      EnqueueOptions options, Object[] args) throws Exception {
+      EnqueueOptions options, Object[] args) {
 
     return DBOSExecutor.enqueueWorkflow(
         Objects.requireNonNull(
@@ -212,6 +290,13 @@ public class DBOSClient implements AutoCloseable {
         null);
   }
 
+  /**
+   * Send a message to a workflow
+   * @param destinationId workflowId of the workflow to receive the message
+   * @param message Message contents
+   * @param topic Topic for the message
+   * @param idempotencyKey If specified, use the value to ensure exactly-once send semantics
+   */
   public void send(String destinationId, Object message, String topic, String idempotencyKey) {
     var workflowId = "%s-%s".formatted(destinationId, idempotencyKey);
     if (idempotencyKey == null) {
@@ -225,41 +310,88 @@ public class DBOSClient implements AutoCloseable {
     systemDatabase.send(status.workflowId(), 0, destinationId, message, topic);
   }
 
+  /**
+   * Get event from a workflow, or null if the operation times out
+   * @param targetId ID of the workflow setting the event
+   * @param key Key for the event
+   * @param timeout Maximum time duration to wait before returning `null`
+   * @return Workflow event value, or `null` if the timeout is hit.
+   */
   public Object getEvent(String targetId, String key, Duration timeout) {
     return systemDatabase.getEvent(targetId, key, timeout, null);
   }
 
+  /**
+   * Create a handle for a workflow.
+   *   This call does not ensure that the workflow exists; use the returned handle's `getStatus()`.
+   * @param <T> Type of the workflow's return value
+   * @param <E> Type of any checked exception thrown by the workflow
+   * @param workflowId ID of the workflow to retrieve
+   * @return A `WorkflowHandle` for the specified worflow ID
+   */
   public <T, E extends Exception> WorkflowHandle<T, E> retrieveWorkflow(String workflowId) {
     return new WorkflowHandleClient<T, E>(workflowId);
   }
 
+  /**
+   * Cancel a worflow
+   * @param workflowId ID of the workflow to cancel
+   */
   public void cancelWorkflow(String workflowId) {
     systemDatabase.cancelWorkflow(workflowId);
   }
 
+  /**
+   * Resume a canceled workflow, providing a handle to the workflow
+   * @param <T> Type of the workflow's return value
+   * @param <E> Type of any checked exception thrown by the workflow
+   * @param workflowId ID of the workflow to resume
+   * @return `WorkflowHandle` for the resumed workflow
+   */
   public <T, E extends Exception> WorkflowHandle<T, E> resumeWorkflow(String workflowId) {
     systemDatabase.resumeWorkflow(workflowId);
     return retrieveWorkflow(workflowId);
   }
 
+  /**
+   * Fork a workflow, providing a handle to the new workflow
+   * @param <T> Type of the workflow's return value
+   * @param <E> Type of any checked exception thrown by the workflow
+   * @param originalWorkflowId ID of the workflow to fork
+   * @param startStep Step number for starting the new fork of the workflow; if zero start from the beginning
+   * @param options Options for forking; 
+   * @return `WorkflowHandle` for the new workflow
+   */
   public <T, E extends Exception> WorkflowHandle<T, E> forkWorkflow(
       String originalWorkflowId, int startStep, ForkOptions options) {
     var forkedWorkflowId = systemDatabase.forkWorkflow(originalWorkflowId, startStep, options);
     return retrieveWorkflow(forkedWorkflowId);
   }
 
+  /**
+   * Get the status of a workflow
+   * @param workflowId ID of the workflow to query for status
+   * @return WorkflowStatus of the workflow, or empty if the workflow does not exist
+   */
   public Optional<WorkflowStatus> getWorkflowStatus(String workflowId) {
     return Optional.ofNullable(systemDatabase.getWorkflowStatus(workflowId));
   }
 
+  /**
+   * List workflows matching the supplied input filter criteria
+   * @param input Filter criteria to use for listing workflows
+   * @return list of workflows matching the `ListWorkflowsInput` criteria
+   */
   public List<WorkflowStatus> listWorkflows(ListWorkflowsInput input) {
     return systemDatabase.listWorkflows(input);
   }
 
+  /**
+   * List the steps executed by a workflow
+   * @param workflowId ID of the workflow to list
+   * @return List of steps executed by the workflow
+   */
   public List<StepInfo> listWorkflowSteps(String workflowId) {
     return systemDatabase.listWorkflowSteps(workflowId);
   }
-
-  // readStream
-
 }
--- src/main/java/dev/dbos/transact/workflow/ForkOptions.java
+++ src/main/java/dev/dbos/transact/workflow/ForkOptions.java
@@ -2,6 +2,12 @@ package dev.dbos.transact.workflow;
 
 import java.time.Duration;
 
+/**
+ * Options for forking a workflow.  This includes:
+ *   Specified ID for the new workflow
+ *   Application version to use for executing the new workflow
+ *   Timeout to apply for the new workflow execution
+ */
 public record ForkOptions(String forkedWorkflowId, String applicationVersion, Duration timeout) {
 
   public ForkOptions {
@@ -14,7 +20,32 @@ public record ForkOptions(String forkedWorkflowId, String applicationVersion, Du
     this(null, null, null);
   }
 
+  /** Assign the workflow ID for the new workflow */
   public ForkOptions(String forkedWorkflowId) {
     this(forkedWorkflowId, null, null);
+  }
+
+  /**
+   * Returns a copy of this object with the given forkedWorkflowId.
+   * @param forkedWorkflowId ID to assign to the forked workflow.
+   */
+  public ForkOptions withForkedWorkflowId(String forkedWorkflowId) {
+    return new ForkOptions(forkedWorkflowId, this.applicationVersion, this.timeout);
+  }
+
+  /**
+   * Returns a copy of this object with the given applicationVersion.
+   * @param applicationVersion Application version to use for the new fork of the workflow
+   */
+  public ForkOptions withApplicationVersion(String applicationVersion) {
+    return new ForkOptions(this.forkedWorkflowId, applicationVersion, this.timeout);
+  }
+
+  /**
+   * Returns a copy of this object with the given timeout.
+   * @param timeout Duration to allow for the workflow to run, before canceling the workflow
+   */
+  public ForkOptions withTimeout(Duration timeout) {
+    return new ForkOptions(this.forkedWorkflowId, this.applicationVersion, timeout);
   }
 }
