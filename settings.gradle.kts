c3e544b9b046d8bc87ea48cc2debe91d0dd1fa81
Harry Pierson <harry.pierson@dbos.dev>
2025-10-28 12:54:01 -0700

    fix gradle 10 incompat warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
 rootProject.name = "transact"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version "1.0.0"
+}
+
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+            }
+        }
+    }
+}
