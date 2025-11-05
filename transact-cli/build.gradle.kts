0c887d8d27fea6c25ac98ae110c47ae84386fa9f
Harry Pierson <harry.pierson@dbos.dev>
2025-11-05 15:50:55 -0800

    add PMD and Spotless to cli project

--- transact-cli/build.gradle.kts
+++ transact-cli/build.gradle.kts
@@ -3,11 +3,37 @@ version = rootProject.extra["calculatedVersion"] as String
 
 plugins {
     application
+    id("pmd")
+    id("com.diffplug.spotless") version "8.0.0"
     id("com.gradleup.shadow") version "9.2.2"
 }
 
 application {
     mainClass.set("dev.dbos.transact.cli.DBOSCommandLine")
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        importOrder("dev.dbos", "java", "javax", "")
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
+pmd {
+    ruleSets = listOf() // disable defaults
+    ruleSetFiles = files("config/pmd/ruleset.xml")
+    isConsoleOutput = true
+    toolVersion = "7.16.0"
+}
+
+tasks.withType<Pmd> {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 repositories {
