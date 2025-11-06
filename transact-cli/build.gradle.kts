833b5cb9fe9e9f008361b03dff5dfaedd17ed0d4
Harry Pierson <harry.pierson@dbos.dev>
2025-11-06 13:55:52 -0800

    remove redundancy

--- transact-cli/build.gradle.kts
+++ transact-cli/build.gradle.kts
@@ -1,36 +1,10 @@
 plugins {
     application
-    id("pmd")
-    id("com.diffplug.spotless") version "8.0.0"
     id("com.gradleup.shadow") version "9.2.2"
 }
 
 application {
     mainClass.set("dev.dbos.transact.cli.DBOSCommandLine")
-}
-
-spotless {
-    java {
-        googleJavaFormat()
-        importOrder("dev.dbos", "java", "javax", "")
-        removeUnusedImports()
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
-}
-
-pmd {
-    ruleSets = listOf() // disable defaults
-    ruleSetFiles = files("${rootDir}/config/pmd/ruleset.xml")
-    isConsoleOutput = true
-    toolVersion = "7.16.0"
-}
-
-tasks.withType<Pmd> {
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-    }
 }
 
 dependencies {
