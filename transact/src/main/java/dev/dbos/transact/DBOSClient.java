1816b894af73c6cc4b6ca4bb1d6d8e5b81d5a394
Harry Pierson <harry.pierson@dbos.dev>
2025-11-12 17:40:49 -0800

    javadoc update

--- transact/src/main/java/dev/dbos/transact/DBOSClient.java
+++ transact/src/main/java/dev/dbos/transact/DBOSClient.java
@@ -67,6 +67,7 @@ public class DBOSClient implements AutoCloseable {
    * @param url System database JDBC URL
    * @param user System database user
    * @param password System database credential / password
+   * @param schema Database schema for DBOS tables 
    */
   public DBOSClient(String url, String user, String password, String schema) {
     var dataSource = SystemDatabase.createDataSource(url, user, password, 0, 0);
@@ -86,6 +87,7 @@ public class DBOSClient implements AutoCloseable {
    * Construct a DBOSClient, by providing a configured data source
    *
    * @param dataSource System database data source
+   * @param schema Database schema for DBOS tables
    */
   public DBOSClient(HikariDataSource dataSource, String schema) {
     systemDatabase = new SystemDatabase(dataSource, schema);
