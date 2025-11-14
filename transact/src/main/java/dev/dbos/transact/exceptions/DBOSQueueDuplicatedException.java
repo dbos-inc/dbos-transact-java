b5825c501796bf1c04a1f4f3285d3a03e3a32c50
Chuck B <chuck.bear@dbos.dev>
2025-11-14 11:52:27 -0500

    Doc exceptions

--- transact/src/main/java/dev/dbos/transact/exceptions/DBOSQueueDuplicatedException.java
+++ transact/src/main/java/dev/dbos/transact/exceptions/DBOSQueueDuplicatedException.java
@@ -1,5 +1,10 @@
 package dev.dbos.transact.exceptions;
 
+/**
+ * {@code DBOSQueueDuplicatedException} is thrown when an attempt is made to enqueue a workflow into
+ * a queue with deduplication. DBOSQueueDuplicatedException is thrown to indicate that a queued
+ * workflow with this `deduplicationId` already exists.
+ */
 public class DBOSQueueDuplicatedException extends RuntimeException {
   private final String workflowId;
   private final String queueName;
@@ -15,14 +20,20 @@ public class DBOSQueueDuplicatedException extends RuntimeException {
     this.deduplicationId = deduplicationId;
   }
 
+  /** ID of the workflow for which enqueue was attempted (This is not the existing workflow ID) */
   public String workflowId() {
     return workflowId;
   }
 
+  /** The name of the queue into which enqueueing was attempted */
   public String queueName() {
     return queueName;
   }
 
+  /**
+   * The deduplication ID within the queue; a workflow with this deduplication ID was already
+   * enqueued.
+   */
   public String deduplicationId() {
     return deduplicationId;
   }
--- transact/src/main/java/dev/dbos/transact/exceptions/DBOSWorkflowExecutionConflictException.java
+++ transact/src/main/java/dev/dbos/transact/exceptions/DBOSWorkflowExecutionConflictException.java
@@ -1,5 +1,11 @@
 package dev.dbos.transact.exceptions;
 
+/**
+ * {@code DBOSWorkflowExecutionConflictException} is thrown when DBOS detects two concurrent
+ * executions running under the same workflow ID. All but one of the concurrent executions will be
+ * terminated by throwing DBOSWorkflowExecutionConflictException. As this exception indicates that
+ * execution should be abandoned, this exception should not be caught or otherwise handled.
+ */
 public class DBOSWorkflowExecutionConflictException extends RuntimeException {
   private String workflowId;
 
@@ -8,6 +14,7 @@ public class DBOSWorkflowExecutionConflictException extends RuntimeException {
     this.workflowId = workflowId;
   }
 
+  /** ID of the workflow for which concurrent executions were noticed */
   public String workflowId() {
     return workflowId;
   }
