914b2ba1e173291749e283843c2dc4f359c2d7ee
Harry Pierson <harry.pierson@dbos.dev>
2026-01-06 14:55:45 -0800

    fix compile error

--- transact/src/test/java/dev/dbos/transact/queue/QueuesTest.java
+++ transact/src/test/java/dev/dbos/transact/queue/QueuesTest.java
@@ -22,7 +22,6 @@ import dev.dbos.transact.workflow.internal.WorkflowStatusInternal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -681,7 +680,7 @@ public class QueuesTest {
         DBOS.startWorkflow(
             () -> serviceQ.simpleQWorkflow("one"), new StartWorkflowOptions(queueOne));
 
-    Thread.sleep(Duration.ofSeconds(3));
+    Thread.sleep(3000);
     assertEquals("oneone", h1.getResult());
     assertEquals("ENQUEUED", h2.getStatus().status());
   }
