f925c390fbbbfd15e07e67311822f10a6246e858
hannes.oswald <hannes.oswald97@gmail.com>
2026-01-12 21:01:04 +0100

    update testcontainers

--- transact/src/test/java/dev/dbos/transact/DbSetupTestBase.java
+++ transact/src/test/java/dev/dbos/transact/DbSetupTestBase.java
@@ -6,11 +6,11 @@ import dev.dbos.transact.database.SystemDatabase;
 import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 public class DbSetupTestBase {
 
-  protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18");
+  protected static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18");
 
   protected static DBOSConfig dbosConfig;
   protected static HikariDataSource dataSource;
