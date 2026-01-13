c10605a739340991ae821ef264ce98ede7952f37
Harry Pierson <harry.pierson@dbos.dev>
2026-01-12 16:00:49 -0800

    spotless

--- transact/src/test/java/dev/dbos/transact/workflow/TimeoutTest.java
+++ transact/src/test/java/dev/dbos/transact/workflow/TimeoutTest.java
@@ -21,7 +21,6 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @org.junit.jupiter.api.Timeout(value = 2, unit = java.util.concurrent.TimeUnit.MINUTES)
