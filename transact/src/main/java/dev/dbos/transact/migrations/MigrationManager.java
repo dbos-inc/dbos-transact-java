3190ef85d306e2cce6923dfd48b24e0ca6a1bd31
Harry Pierson <harry.pierson@dbos.dev>
2026-02-10 15:58:44 -0800

    add migration 8

--- transact/src/main/java/dev/dbos/transact/migrations/MigrationManager.java
+++ transact/src/main/java/dev/dbos/transact/migrations/MigrationManager.java
@@ -228,7 +228,15 @@ public class MigrationManager {
   public static List<String> getMigrations(String schema) {
     Objects.requireNonNull(schema);
     var migrations =
-        List.of(migration1, migration2, migration3, migration4, migration5, migration6, migration7, migration8);
+        List.of(
+            migration1,
+            migration2,
+            migration3,
+            migration4,
+            migration5,
+            migration6,
+            migration7,
+            migration8);
     return migrations.stream().map(m -> m.formatted(schema)).toList();
   }
 
@@ -399,5 +407,4 @@ public class MigrationManager {
       ALTER TABLE %1$s."workflow_status" ADD COLUMN "parent_workflow_id" TEXT DEFAULT NULL;
       CREATE INDEX "idx_workflow_status_parent_workflow_id" ON %1$s."workflow_status" ("parent_workflow_id");
       """;
-
 }
