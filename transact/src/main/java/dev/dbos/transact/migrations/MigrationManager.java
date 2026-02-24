0845ab7b1a548597d599c876dda6c091a7f18812
Harry Pierson <harry.pierson@dbos.dev>
2026-02-24 10:21:10 -0800

    fix migration 11 schema quoting

--- transact/src/main/java/dev/dbos/transact/migrations/MigrationManager.java
+++ transact/src/main/java/dev/dbos/transact/migrations/MigrationManager.java
@@ -447,11 +447,11 @@ public class MigrationManager {
 
   static final String migration11 =
       """
-      ALTER TABLE %1$s."workflow_status" ADD COLUMN "serialization" TEXT DEFAULT NULL;
-      ALTER TABLE %1$s."notifications" ADD COLUMN "serialization" TEXT DEFAULT NULL;
-      ALTER TABLE %1$s."workflow_events" ADD COLUMN "serialization" TEXT DEFAULT NULL;
-      ALTER TABLE %1$s."workflow_events_history" ADD COLUMN "serialization" TEXT DEFAULT NULL;
-      ALTER TABLE %1$s."operation_outputs" ADD COLUMN "serialization" TEXT DEFAULT NULL;
-      ALTER TABLE %1$s."streams" ADD COLUMN "serialization" TEXT DEFAULT NULL;
+      ALTER TABLE "%1$s"."workflow_status" ADD COLUMN "serialization" TEXT DEFAULT NULL;
+      ALTER TABLE "%1$s"."notifications" ADD COLUMN "serialization" TEXT DEFAULT NULL;
+      ALTER TABLE "%1$s"."workflow_events" ADD COLUMN "serialization" TEXT DEFAULT NULL;
+      ALTER TABLE "%1$s"."workflow_events_history" ADD COLUMN "serialization" TEXT DEFAULT NULL;
+      ALTER TABLE "%1$s"."operation_outputs" ADD COLUMN "serialization" TEXT DEFAULT NULL;
+      ALTER TABLE "%1$s"."streams" ADD COLUMN "serialization" TEXT DEFAULT NULL;
       """;
 }
