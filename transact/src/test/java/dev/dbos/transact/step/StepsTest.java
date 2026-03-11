0d62d9ba7a2a6c1a86be8492d92a40a8d7d2124b
Harry Pierson <harry.pierson@dbos.dev>
2026-03-11 12:21:16 -0700

    consolidation

--- transact/src/test/java/dev/dbos/transact/step/StepsTest.java
+++ transact/src/test/java/dev/dbos/transact/step/StepsTest.java
@@ -11,6 +11,305 @@ import dev.dbos.transact.workflow.*;
 import java.util.List;
 
 import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface ServiceA {
+  String workflowWithSteps(String input);
+
+  String workflowWithStepError(String input);
+}
+
+class ServiceAImpl implements ServiceA {
+
+  private static final Logger logger = LoggerFactory.getLogger(ServiceAImpl.class);
+
+  private final ServiceB serviceBproxy;
+
+  ServiceAImpl(ServiceB b) {
+    this.serviceBproxy = b;
+  }
+
+  @Override
+  @Workflow(name = "workflowWithSteps")
+  public String workflowWithSteps(String input) {
+    serviceBproxy.step1("one");
+    serviceBproxy.step2("two");
+    try {
+      serviceBproxy.step3("three", false);
+    } catch (Exception e) {
+      logger.info(e.getMessage());
+    }
+    serviceBproxy.step4("four");
+    serviceBproxy.step5("five");
+
+    return input + input;
+  }
+
+  @Override
+  @Workflow(name = "workflowWithStepsError")
+  public String workflowWithStepError(String input) {
+    serviceBproxy.step1("one");
+    serviceBproxy.step2("two");
+    try {
+      serviceBproxy.step3("three", true);
+    } catch (Exception e) {
+      logger.info(e.getMessage());
+    }
+    serviceBproxy.step4("four");
+    serviceBproxy.step5("five");
+
+    return input + input;
+  }
+}
+
+interface ServiceB {
+  String step1(String input);
+
+  String step2(String input);
+
+  String step3(String input, boolean throwError) throws Exception;
+
+  String step4(String input);
+
+  String step5(String input);
+}
+
+class ServiceBImpl implements ServiceB {
+
+  @Override
+  @Step(name = "step1")
+  public String step1(String input) {
+    return input;
+  }
+
+  @Override
+  @Step(name = "step2")
+  public String step2(String input) {
+    return input;
+  }
+
+  @Override
+  @Step(name = "step3")
+  public String step3(String input, boolean throwError) throws Exception {
+    if (throwError) {
+      throw new Exception("step3 error");
+    }
+    return input;
+  }
+
+  @Override
+  @Step(name = "step4")
+  public String step4(String input) {
+    return input;
+  }
+
+  @Override
+  @Step(name = "step5")
+  public String step5(String input) {
+    return input;
+  }
+}
+
+interface ServiceWFAndStep {
+  String aWorkflow(String input);
+
+  String stepOne(String input);
+
+  String stepTwo(String input);
+
+  String aWorkflowWithInlineSteps(String input);
+
+  String stepWith2Retries(String input) throws Exception;
+
+  String stepWithNoRetriesAllowed(String input) throws Exception;
+
+  String stepWithLongRetry(String input) throws Exception;
+
+  String stepRetryWorkflow(String input);
+
+  String inlineStepRetryWorkflow(String input);
+}
+
+class ServiceWFAndStepImpl implements ServiceWFAndStep {
+
+  private final DBOS.Instance dbos;
+
+  private ServiceWFAndStep self;
+
+  public ServiceWFAndStepImpl(DBOS.Instance dbos) {
+    this.dbos = dbos;
+  }
+
+  public void setSelf(ServiceWFAndStep serviceWFAndStep) {
+    self = serviceWFAndStep;
+  }
+
+  @Override
+  @Workflow(name = "myworkflow")
+  public String aWorkflow(String input) {
+    String s1 = self.stepOne("one");
+    String s2 = self.stepTwo("two");
+    return input + s1 + s2;
+  }
+
+  @Override
+  @Step(name = "step1")
+  public String stepOne(String input) {
+    return input;
+  }
+
+  @Override
+  @Step(name = "step2")
+  public String stepTwo(String input) {
+    return input;
+  }
+
+  @Override
+  @Workflow(name = "aWorkflowWithInlineSteps")
+  public String aWorkflowWithInlineSteps(String input) {
+    var len = dbos.runStep(() -> input.length(), new StepOptions("stringLength"));
+    return (input + len);
+  }
+
+  int stepWithRetryRuns = 0;
+
+  @Override
+  @Step(
+      name = "stepWith2Retries",
+      retriesAllowed = true,
+      maxAttempts = 2,
+      intervalSeconds = .01,
+      backOffRate = 1)
+  public String stepWith2Retries(String input) throws Exception {
+    ++this.stepWithRetryRuns;
+    throw new Exception("Will not ever run");
+  }
+
+  int stepWithNoRetryRuns = 0;
+
+  @Override
+  @Step(name = "stepWithNoRetriesAllowed", retriesAllowed = false)
+  public String stepWithNoRetriesAllowed(String input) throws Exception {
+    ++stepWithNoRetryRuns;
+    throw new Exception("No retries");
+  }
+
+  long startedTime = 0;
+  int stepWithLongRetryRuns = 0;
+
+  @Override
+  @Step(
+      name = "stepWithLongRetry",
+      maxAttempts = 3,
+      retriesAllowed = true,
+      intervalSeconds = 1,
+      backOffRate = 10)
+  public String stepWithLongRetry(String input) throws Exception {
+    ++stepWithLongRetryRuns;
+    if (startedTime == 0) {
+      startedTime = System.currentTimeMillis();
+      throw new Exception("First try");
+    }
+    if (System.currentTimeMillis() - startedTime > 500) {
+      var rv = Integer.valueOf(this.stepWithLongRetryRuns).toString();
+      startedTime = 0;
+      return rv;
+    }
+    throw new Exception("Not enough time passed yet");
+  }
+
+  @Override
+  @Workflow(name = "retryTestWorkflow")
+  public String stepRetryWorkflow(String input) {
+    long ctime = System.currentTimeMillis();
+    boolean caught = false;
+    String result = "2 Retries: ";
+    try {
+      result = result + self.stepWith2Retries(input);
+    } catch (Exception e) {
+      caught = true;
+    }
+    if (!caught) {
+      result += "<Step with retries should have thrown>";
+    }
+    if (System.currentTimeMillis() - ctime > 1000) {
+      result += "<Retry took too long>";
+    }
+    result += this.stepWithRetryRuns;
+    result += ".";
+
+    result += "  No retry: ";
+    caught = false;
+    try {
+      self.stepWithNoRetriesAllowed("");
+    } catch (Exception e) {
+      caught = true;
+    }
+    if (!caught) {
+      result += "<Step with no retries should have thrown>";
+    }
+    result += this.stepWithNoRetryRuns;
+    result += ".";
+
+    ctime = System.currentTimeMillis();
+    result += "  Backoff timeout: ";
+    caught = false;
+    try {
+      self.stepWithLongRetry("");
+    } catch (Exception e) {
+      caught = true;
+    }
+    if (caught) {
+      result += "<Step with long retry should have completed>";
+    }
+    if (System.currentTimeMillis() - ctime > 2000) {
+      result += "<Step with long retry should have finished faster>";
+    }
+    if (System.currentTimeMillis() - ctime < 500) {
+      result += "<Step with long retry should does not appear to have slept>";
+    }
+    result += this.stepWithLongRetryRuns;
+    result += ".";
+
+    return result;
+  }
+
+  @Override
+  @Workflow(name = "inlineStepRetryTestWorkflow")
+  public String inlineStepRetryWorkflow(String input) {
+    long ctime = System.currentTimeMillis();
+    boolean caught = false;
+    String result = "2 Retries: ";
+    try {
+      result =
+          result
+              + dbos.runStep(
+                  () -> {
+                    ++this.stepWithRetryRuns;
+                    throw new Exception("Will not ever run");
+                  },
+                  new StepOptions("inlineStepWithRetries")
+                      .withRetriesAllowed(true)
+                      .withMaxAttempts(2)
+                      .withIntervalSeconds(0.01)
+                      .withBackoffRate(2.0));
+      ;
+    } catch (Exception e) {
+      caught = true;
+    }
+    if (!caught) {
+      result += "<Step with retries should have thrown>";
+    }
+    if (System.currentTimeMillis() - ctime > 1000) {
+      result += "<Retry took too long>";
+    }
+    result += this.stepWithRetryRuns;
+    result += ".";
+
+    return result;
+  }
+}
 
 @org.junit.jupiter.api.Timeout(value = 2, unit = java.util.concurrent.TimeUnit.MINUTES)
 @org.junit.jupiter.api.parallel.Execution(org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT)
