4c71ec53b0e4ff6a76965c10e29df1cd83d988bb
Harry Pierson <harry.pierson@dbos.dev>
2026-04-22 12:31:32 -0700

    spotless

--- transact/src/test/java/dev/dbos/transact/queue/QueuesTest.java
+++ transact/src/test/java/dev/dbos/transact/queue/QueuesTest.java
@@ -2,7 +2,6 @@ package dev.dbos.transact.queue;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
