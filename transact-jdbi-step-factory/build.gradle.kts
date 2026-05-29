e269f46b3a5e993a28e7cdd8060edc056e951abd
Harry Pierson <harry.pierson@dbos.dev>
2026-05-28 20:13:37 -0700

    publish JDBI/JOOQ step factory JARs

--- transact-jdbi-step-factory/build.gradle.kts
+++ transact-jdbi-step-factory/build.gradle.kts
@@ -1,10 +1,62 @@
-plugins { id("java-library") }
+import com.vanniktech.maven.publish.DeploymentValidation
+
+plugins {
+  id("java-library")
+  alias(libs.plugins.maven.publish)
+}
 
 tasks.withType<JavaCompile> {
   options.compilerArgs.add("-Xlint:unchecked")
   options.compilerArgs.add("-Xlint:deprecation")
   options.compilerArgs.add("-Xlint:rawtypes")
   options.compilerArgs.add("-Werror")
+}
+
+tasks.withType<Javadoc> {
+  (options as StandardJavadocDocletOptions).apply {
+    addStringOption("Xdoclint:all,-missing", "-quiet")
+    encoding = "UTF-8"
+  }
+}
+
+tasks.named("build") { dependsOn("javadoc") }
+
+val publishingToMavenCentral =
+  gradle.startParameter.taskNames.any { it.contains("publishToMavenCentral") }
+
+mavenPublishing {
+  publishToMavenCentral(automaticRelease = true, validateDeployment = DeploymentValidation.NONE)
+  if (publishingToMavenCentral) {
+    signAllPublications()
+  }
+
+  pom {
+    name.set("DBOS Transact JDBI Step Factory")
+    description.set("JDBI step factory for DBOS Transact Java SDK")
+    inceptionYear.set("2025")
+    url.set("https://github.com/dbos-inc/dbos-transact-java")
+
+    licenses {
+      license {
+        name.set("MIT License")
+        url.set("https://opensource.org/licenses/MIT")
+      }
+    }
+
+    developers {
+      developer {
+        id.set("dbos-inc")
+        name.set("DBOS Inc")
+        email.set("support@dbos.dev")
+      }
+    }
+
+    scm {
+      connection.set("scm:git:git://github.com/dbos-inc/dbos-transact-java.git")
+      developerConnection.set("scm:git:ssh://github.com:dbos-inc/dbos-transact-java.git")
+      url.set("https://github.com/dbos-inc/dbos-transact-java/tree/main")
+    }
+  }
 }
 
 dependencies {
