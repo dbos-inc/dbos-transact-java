cc097c4811c4cf19ffd096afaa6ba8126688f684
Max dml <maxdml@dbos.dev>
2026-09-15 08:40:52 -0700

    VMID fixes (#497)
    - Set to local when DBOS__VMID is set but empty
    - Don't regenerate an executor ID when a conductor key is provided in
    the code config and we are in DBOS Cloud

--- transact/src/main/java/dev/dbos/transact/execution/DBOSExecutor.java
+++ transact/src/main/java/dev/dbos/transact/execution/DBOSExecutor.java
@@ -185,7 +185,8 @@ public class DBOSExecutor implements AutoCloseable {
             ? Objects.requireNonNullElse(System.getenv("DBOS_APP_NAME"), "")
             : config.appName();
     appVersion = Objects.requireNonNullElse(System.getenv("DBOS__APPVERSION"), "");
-    executorId = Objects.requireNonNullElse(System.getenv("DBOS__VMID"), "local");
+    var envExecutorId = System.getenv("DBOS__VMID");
+    executorId = envExecutorId == null || envExecutorId.isEmpty() ? "local" : envExecutorId;
 
     if (appName.isEmpty()) {
       var msg =
@@ -218,6 +219,9 @@ public class DBOSExecutor implements AutoCloseable {
       }
       if (config.executorId() != null) {
         executorId = config.executorId();
+      }
+      if (config.conductorKey() != null) {
+        executorId = UUID.randomUUID().toString();
       }
     }
   }
@@ -256,10 +260,6 @@ public class DBOSExecutor implements AutoCloseable {
         this.appVersion =
             AppVersionComputer.computeAppVersion(
                 DBOS.version(), this.appName, workflowMap.values());
-      }
-
-      if (config.conductorKey() != null) {
-        this.executorId = UUID.randomUUID().toString();
       }
 
       logger.info("System Database: {}", this.config.databaseUrl());
